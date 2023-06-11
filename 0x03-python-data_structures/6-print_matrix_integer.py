fbf8d700b76acddf93c13f3916546d128cb0d66c
CloudyHeaveen <abirarahou2002@gmail.com>
2023-06-11 09:45:30 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - ARAHOU ABIR
+def print_matrix_integer(matrix=[[]]):
+    for a in b:
+        for c in a:
+            if c == a[-1]:
+                print('{:d}'.format(c), end='')
+            else:
+                print('{:d}'.format(c), end=' ')
+        print()
