0483dab35f4ac3095eb755f5531337d1dacb4428
CloudyHeaveen <abirarahou2002@gmail.com>
2023-06-14 17:52:44 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,10 @@
 # Author - ARAHOU ABIR
 def weight_average(my_list=[]):
     if my_list and len(my_list):
-        number = 0
+        num = 0
         denom = 0
         for tupl in my_list:
             num += (tupl[0] * tupl[1])
             denom += (tupl[1])
-        return (number/denom)
+        return (num/denom)
     return 0
