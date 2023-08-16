35b8a002000249bea61b1790a8d19a05adffe069
CloudyHeaveen <abirarahou2002@gmail.com>
2023-08-15 17:43:08 -0700

    ARAHOU ABIR

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
--- Converts hbtn_0c_0 database to utf8
--- Table: first_table to utf8
--- Field: name to utf8
+-- converts hbtn_0c_0 database to utf8
+-- table: first_table to utf8
+-- field: name to utf8
 
 ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
