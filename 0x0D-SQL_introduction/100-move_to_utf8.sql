6158c028da5c492becc57158a0d772802b4952ab
CloudyHeaveen <abirarahou2002@gmail.com>
2023-08-15 17:45:52 -0700

    ARAHOU ABIR

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,15 @@
--- Converts hbtn_0c_0 database to utf8
--- Table: first_table to utf8
--- Field: name to utf8
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Connect to your MySQL server and authenticate if necessary
+
+-- Convert the database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Use the database
+USE hbtn_0c_0;
+
+-- Convert the table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the field
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Disconnect from the MySQL server
