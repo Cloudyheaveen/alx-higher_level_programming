24b08843ccc4c7e69fcbf8fa7c312e65ef79924f
CloudyHeaveen <abirarahou2002@gmail.com>
2023-09-17 05:24:08 -0700

    ARAHOU ABIR

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A script that lists all State objects from the database hbtn_0e_6_usa """:
+""" A script that lists all State objects from the database hbtn_0e_6_usa """
 
 from sys import argv
 from model_state import Base, State
