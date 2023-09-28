b0d848f2e661a481e6d717a6340f0a0f3567b866
CloudyHeaveen <abirarahou2002@gmail.com>
2023-09-28 16:15:09 -0700

    ARAHOU ABIR

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
+# A bash script that takes in a URL, sends a GET request to the URL
 curl -sL "$1"
